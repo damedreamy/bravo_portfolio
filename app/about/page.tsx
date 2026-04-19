2d702675d38852d811be58726d1deba36dbb2a7a
Rencess <rencessdame18@gmail.com>
2026-04-19 16:59:57 +0800

    Create page.tsx
    update about

---
+++ app/about/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+export default function AboutMe() {
+    return (
+        <main>
+            <h2>About Me</h2>
+
+
+            <Image src="/Bravo.jpg" alt="Rencess Dame Bravo" width={200} height={200} />
+
+            <p>
+                Hello World! I’m Rencess Dame Bravo, a 20-year-old developer passionate about creating websites, apps, databases, and intuitive user interfaces that function smoothly.
+
+                Ive participated in school projects, hackathons, and seminars, honing my problem-solving, teamwork, and development skills. I love exploring new tech and transforming ideas into usable digital experiences.
+
+                Still learning, still building, and leveling up—one project at a time.
+            </p>
+
+
+
+
+        </main>
+    );
+}

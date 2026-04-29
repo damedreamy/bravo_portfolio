d56ba6e523bfff1269bac380980076ee5babbb2e
Dame <rencessdame18@gmail.com>
2026-04-30 01:43:17 +0800

    Update vortex.tsx
    update

--- components/ui/vortex.tsx
+++ components/ui/vortex.tsx
@@ -21,8 +21,7 @@ interface VortexProps {
 export const Vortex = (props: VortexProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const containerRef = useRef(null);
-  const animationFrameId = useRef<number>();
-  const particleCount = props.particleCount || 700;
+const animationFrameId = useRef<number | null>(null);  const particleCount = props.particleCount || 700;
   const particlePropCount = 9;
   const particlePropsLength = particleCount * particlePropCount;
   const rangeY = props.rangeY || 100;
